5fad46dc32ee35569f12f521bfbae9d8d7ae4696
Rajeev Kumar <rajeevku02@gmail.com>
2021-11-22 13:33:38 +0530

    Few geometric utils

--- ml/geometry.py
+++ ml/geometry.py
@@ -1,5 +1,7 @@
 import math
 
+RANGE_DELTA = 0.4
+
 def dist(p1, p2):
     dx = p2.x - p1.x
     dy = p2.y - p1.y
@@ -43,7 +45,52 @@ class LineEq:
         d2 = self.A * p2.x + self.B * p2.y + self.C
         return d1 * d2 >= 0
 
+    def perp(self, p):
+        eq = LineEq(Point(0, 0), Point(1, 1))
+        eq.A = -self.B
+        eq.B = self.A
+        eq.C = -eq.A * p.x - eq.B * p.y
+        return eq
+
 class LineSegment:
     def __init__(self, p1, p2):
         self.p1 = p1
         self.p2 = p2
+
+    def length(self):
+        return dist(self.p1, self.p2)
+
+    def eq(self):
+        return LineEq(self.p1, self.p2)
+
+    def contains(self, pt):
+        d1 = dist(pt, self.p1)
+        d2 = dist(pt, self.p2)
+        len = self.length()
+        diff = abs(len - (d1 + d2))
+        diff < RANGE_DELTA
+
+    def dist_from(self, point):
+        eq = LineEq(self.p1, self.p2)
+        eq2 = eq.perp(point)
+        _, near = eq.solve(eq2)
+        if self.contains(near):
+            return eq.dist_from(point)
+        d1 = dist(self.p1, point)
+        d2 = dist(self.p2, point)
+        return min(d1, d2)
+
+    def intersect(self, seg):
+        eq1 = LineEq(self.p1, self.p2)
+        eq2 = LineEq(seg.p1, seg.p2)
+        got, at = eq1.solve(eq2)    
+        if not got or not self.contains(at) or not seg.contains(at):
+            return False, Point(0, 0)
+        return True, at
+
+    def intersect_eq(self, eq):
+        eq1 = LineEq(self.p1, self.p2)
+        got, at = eq1.solve(eq)    
+        if not got or not self.contains(at):
+            return False, Point(0, 0)
+        return True, at

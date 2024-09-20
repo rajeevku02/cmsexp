8f1740a4028779e0423600c3ad8313fad810699b
Rajeev Kumar <rajeevku02@gmail.com>
2024-09-20 13:29:11 +0530

    changes

--- pytorch/gpt/main.py
+++ pytorch/gpt/main.py
@@ -2,15 +2,29 @@ from train import train
 from eval import generate
 from model import GPT
 from config import config
+import sys
 
 import torch
 
 torch.manual_seed(427)
 
+def load_and_infer():
+    model = GPT(config)
+    model.load_state_dict(torch.load('./data/savedm/model_weights.pth', weights_only=True))
+    model.eval()
+    while True:
+        text = input("prompt: ")
+        if text == 'exit':
+            return
+        generate(model, text, 40)
+
 def main():
     model = GPT(config)
-    #generate(model, 'hello world', 40)
+    if len(sys.argv) > 1 and sys.argv[1] == 'load':
+        load_and_infer()
+        return
     train(model, config)
+    torch.save(model.state_dict(), './data/savedm/model_weights.pth')
 
 if __name__ == '__main__':
     main()

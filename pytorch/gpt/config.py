c86852f20c61a41c6df6299b32d7712525e32ba7
Rajeev Kumar <rajeevku02@gmail.com>
2024-09-20 12:21:19 +0530

    config changes

--- pytorch/gpt/config.py
+++ pytorch/gpt/config.py
@@ -2,11 +2,13 @@ import tiktoken
 
 class Config:
     def __init__(self):
-        self.max_sequence_len = 4
-        self.n_heads = 2
-        self.embed_dim = 8
+        self.max_sequence_len = 512
+        self.stride = 128
+        self.batch_size = 8
+        self.n_heads = 12
+        self.embed_dim = 768
         self.n_layers = 2
-        self.dropout = 0
+        self.dropout = 0.1
         self.ma_bias = False
         self.tokenizer = tiktoken.get_encoding('gpt2')
         self.n_vocab = self.tokenizer.n_vocab
